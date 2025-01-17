4fe24d5718dfe5abde22eb95a4f7071818aba791
arinrahman <rahman.arin29@gmail.com>
2025-01-17 16:10:35 -0700

    control flow with if else conditional operators

---
+++ task.py
@@ -0,0 +1,8 @@
+#control flow with if else conditional operators
+print("Welcome to Luna Park! Excited for a rollercoaster ride?")
+h=float(input("Please enter your height in cm to check eligibility for this ride.\n"))
+if h>120:
+    print("Have a fun rollercoaster ride!")
+
+else:
+    print("Sorry, you can't ride.")
